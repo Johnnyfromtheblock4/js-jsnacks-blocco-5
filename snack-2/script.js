870343cbabe58560fa4730522f5e455926ce363e
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-17 16:32:02 +0200

    [UPD] snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,16 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
+
+const nameForEach = [];
+
+people.forEach((elem) => {
+  nameForEach.push(`${elem.name}`);
+});
+
+console.log(nameForEach);
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
