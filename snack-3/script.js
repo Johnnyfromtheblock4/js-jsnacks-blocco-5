3a464e11873a52bc6f6872f7e2e6f67905f84069
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-17 16:36:41 +0200

    [UPD] snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,11 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const arrayPlusOne = [];
+
+numbers.forEach((elem) => {
+  arrayPlusOne.push(elem + 1);
+});
+
+console.log(arrayPlusOne);
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
