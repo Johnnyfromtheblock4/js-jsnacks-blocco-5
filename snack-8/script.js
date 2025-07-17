93d0e42fcc131406f94f0ad233b5abf9b764bbba
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-17 17:56:56 +0200

    [UPD] snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,34 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
+
+//cerco l'array di Marco Lanci
+const lanciObj = students.find((student) => {
+  if (student.name === `Marco Lanci`) {
+    return true;
+  }
+});
+
+console.log(lanciObj.class);
+
+//cerco la classe di Marco Lanci
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
